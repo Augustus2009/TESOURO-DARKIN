2defba5d4ee2eda30e8c8c76244e7d544f925556
Augustus2009 <126513026+Augustus2009@users.noreply.github.com>
2023-09-26 13:41:43 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "YOMMI";
+const accessCode2 = "ATROX";
+const accessCode3 = "NAAFIRI";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("O M Y M I", 100,50)
+    fill("lightblue")
+    text("Dica: RIMA COM DESTRUIR!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("X T A O R", 700,150)
+    fill("lightblue")
+    text("Dica: SALVE PANTEON!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A I F R A I", 100,250)
+    fill("lightblue")
+    text("Dica: É UM BOM GAROTO!", 100,270)
+
+}
